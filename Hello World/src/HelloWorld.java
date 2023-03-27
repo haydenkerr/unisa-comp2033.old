62239e3caaf150346e35ed47e79e8093afd7aa46
hayde <hayde@HK-SE-SURFACEPRO8>
2023-03-28 07:19:31 +1100

    commit message for the helloworld

---
+++ Hello World/src/HelloWorld.java
@@ -0,0 +1,13 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HelloWorld hw = new HelloWorld();
+		hw.printHello();
+	}
+
+	public void printHello() {
+		System.out.println("Hello, World!");
+	}
+}
