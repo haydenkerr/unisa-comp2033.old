2e71693e6ca8d35582e281e0ea3a61ace53b76e9
Hayden Kerr <kerhy006@mymail.unisa.edu.au>
2023-04-11 21:00:10 +1000

    updated all and pushing to git

---
+++ week2/Week2_1/src/Week2_1.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author hayde
+ *
+ */
+public class Week2_1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
